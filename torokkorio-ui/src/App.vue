428dca634f8d5cc922875022226d11246d3794ac
pinkikki <w-mizutani@pinkikki.jp>
2018-06-24 22:19:02 +0900

    vue.jsの環境設定追加

---
+++ torokkorio-ui/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Monetization</p>
+    <button @click.prevent="fetch()">fetch</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+  export default {
+    methods: {
+      fetch() {
+        axios.get("http://localhost:5000").then((response) => {
+          console.log(response.data);
+          alert(response.data);
+        }).catch((error) => {
+          console.log(error.data);
+          alert(error.data);
+        });
+      }
+    }
+  }
+</script>
